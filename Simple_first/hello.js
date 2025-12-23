b1e5665fbfb998aadf373cd14f946003847cbe17
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-12-23 16:55:29 +0530

    Basic program

---
+++ Simple_first/hello.js
@@ -0,0 +1,40 @@
+
+//readymade function provided
+//in javascript execution:
+//source code is interpreted by Node JS runtime
+//No code is pre compiled:
+
+var company="Transflower Learning Pvt Ltd";
+var count=45;
+var status=false;
+console.log("hello world");
+console.log(company);
+console.log(count);
+console.log(status);
+
+//json
+//javascript object notation
+var flower={
+    id:22,
+    title:"Gerbera",
+    description:"Wedding flower",
+    availabile:true,
+    unitorice:12,
+    quantity:5000,
+    likes:12000
+};
+console.log(flower.title+" "+flower.description);
+
+var flowers=[]; //empty array
+var freshFlowers=["Rose","Gerbera","Jasmine"]; //array with strings
+var marks=[23,54,76,23];
+var flowers = [
+  { id: 1, title: "Rose", description: "Wedding flower", available: true, unitprice: 15, quantity: 4000, likes: 15000 },
+  { id: 2, title: "Lily", description: "Wedding flower", available: true, unitprice: 18, quantity: 3000, likes: 12000 },
+  { id: 3, title: "Orchid", description: "Wedding flower", available: true, unitprice: 25, quantity: 2000, likes: 18000 },
+  { id: 4, title: "Tulip", description: "Wedding flower", available: true, unitprice: 14, quantity: 3500, likes: 11000 },
+  { id: 5, title: "Carnation", description: "Wedding flower", available: true, unitprice: 10, quantity: 5000, likes: 9000 },
+  { id: 6, title: "Sunflower", description: "Wedding flower", available: true, unitprice: 12, quantity: 4500, likes: 13000 }
+];
+
+console.log(flowers); 
