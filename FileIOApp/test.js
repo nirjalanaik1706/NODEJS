8e5040c685b179fbdd752636f7f0c5f047900fe0
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-12-30 18:24:01 +0530

    change

--- FileIOApp/test.js
+++ FileIOApp/test.js
@@ -2,14 +2,15 @@
 var fs=require('fs');
 var fileName="./data/result.txt";
 
-
+//callback function
 var onFileRead=function(err,data){
 
     if(err){
-        console.log("something has gone wrong");
+        throw err;
     }
 
     console.log("Data from file available");
     console.log(data);
 };
-fs.readFile(fileName,onFileRead);
+fs.readFile(fileName,onFileRead); //non-blocking call
+console.log("terminating Node js application");
