c1fcda5c544a5aa2cd316d564d404549c6dc8ace
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-12-31 19:12:28 +0530

    api added

--- ExpressApp/server.js
+++ ExpressApp/server.js
@@ -2,16 +2,37 @@ var express=require('express');
 var path=require('path');
 var app=express();
 
-app.use(express.static(path.join(__dirname,'public')));
-
-app.listen(8888,()=>{
-    console.log("server is listening on port no. 8888");
-});
+var staticMiddleware=express.static(path.join(__dirname,'public'));
+app.use(staticMiddleware);
 
 app.get((request,response)=>{
     response.sendFile("/index.html");
 });
 
+app.get("/flower",(request,response)=>{
+    var flower={
+        "id":34,
+        "title":"Gerbera",
+        "unit price":5
+    };
+    response.send(flower);
+});
+
+app.get("/flowers",(request,response)=>{
+    var flowers=[
+        { "id":34,"title":"Gerbera","unit price":5 },
+        { "id":35,"title":"rose","unit price":3 },
+        { "id":36,"title":"lily","unit price":2 },
+        { "id":37,"title":"lotus","unit price":7 },
+        { "id":38,"title":"Gerbera","unit price":6 }
+    ]
+    response.send(flowers);
+});
+
 app.get("/family",(request,response)=>{
     response.sendFile(path.join(__dirname,"/public/family.json"));
+});
+
+app.listen(8888,()=>{
+    console.log("server is listening on port no. 8888");
 });
