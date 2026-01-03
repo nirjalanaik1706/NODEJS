56e38ea1fc761970ae2da0138080e552be128536
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-01-03 14:33:10 +0530

    changes

--- CRUD_AJAX_RepositoryPattern/server.js
+++ CRUD_AJAX_RepositoryPattern/server.js
@@ -1,5 +1,19 @@
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(7777, () => {
     console.log("Server listening on port 7777");
 });
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/', userRoutes);
+
+module.exports = app;
+
